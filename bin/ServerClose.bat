9350f33cb13d7ac7f966240c3a37a1800f4eea4d
alenming <635459675@qq.com>
2018-01-09 10:32:25 +0800

    daf
    commit

--- bin/ServerClose.bat
+++ bin/ServerClose.bat
@@ -1,2 +1,8 @@
 taskkill /f /t /im SessionServer.exe
-taskkill /f /t /im TestClient.exe
+taskkill /f /t /im ChatServer.exe
+taskkill /f /t /im BattleServer.exe
+taskkill /f /t /im CheckServer.exe
+taskkill /f /t /im ServiceServer.exe
+taskkill /f /t /im MatchServer.exe
+taskkill /f /t /im UnionServer.exe
+taskkill /f /t /im CenterServer.exe
